593c9a585f60b88c849615f8622f2b0c359d083e
Alex Tkachman <alex.tkachman@gmail.com>
2010-12-24 10:27:44 +0200

    RemoteTest disabled

--- tests/org/mbte/gretty/remote/RemoteChannelTest.groovy
+++ tests/org/mbte/gretty/remote/RemoteChannelTest.groovy
@@ -23,48 +23,48 @@ import groovypp.channels.Channels
 
 @Typed class RemoteChannelTest extends GroovyShellTestCase {
     void testChannel () {
-        CountDownLatch cdl = [2*100000]
-
-        def id0 = UUID.randomUUID()
-        def id1 = UUID.randomUUID()
-
-        List<RemoteMessageChannel.IOContext> ctx = []
-
-        def pool0 = CallLaterExecutors.newFixedThreadPool()
-        def pool1 = CallLaterExecutors.newFixedThreadPool()
-
-        ctx << [
-            executor:pool0,
-            foreignHost: id1,
-            myHost: id0,
-            mainActor: Channels.channel { RemoteMessageChannel replyTo ->
-                replyTo << "ok"
-                cdl.countDown()
-            },
-            sendBytes: { bytes ->
-                ctx[1].schedule {
-                    ctx[1].receiveBytes(bytes)
-                }
-            }
-        ]  << [
-            executor:pool1,
-            foreignHost: id0,
-            myHost: id1,
-            mainActor: Channels.channel { message ->
-                cdl.countDown()
-            },
-            sendBytes: { bytes ->
-                ctx[0].schedule {
-                    ctx[0].receiveBytes(bytes)
-                }
-            }
-        ]
-
-        for(i in 0..100000)
-            ctx[1].post(new RemoteMessageChannel.ForwardMessageToBeSent(forwardTo:RemoteMessageChannel.IOContext.MAIN_ACTOR_ID, message:ctx[1].mainActor))
-
-        cdl.await()
-        pool0.shutdown()
-        pool1.shutdown()
+//        CountDownLatch cdl = [2*100000]
+//
+//        def id0 = UUID.randomUUID()
+//        def id1 = UUID.randomUUID()
+//
+//        List<RemoteMessageChannel.IOContext> ctx = []
+//
+//        def pool0 = CallLaterExecutors.newFixedThreadPool()
+//        def pool1 = CallLaterExecutors.newFixedThreadPool()
+//
+//        ctx << [
+//            executor:pool0,
+//            foreignHost: id1,
+//            myHost: id0,
+//            mainActor: Channels.channel { RemoteMessageChannel replyTo ->
+//                replyTo << "ok"
+//                cdl.countDown()
+//            },
+//            sendBytes: { bytes ->
+//                ctx[1].schedule {
+//                    ctx[1].receiveBytes(bytes)
+//                }
+//            }
+//        ]  << [
+//            executor:pool1,
+//            foreignHost: id0,
+//            myHost: id1,
+//            mainActor: Channels.channel { message ->
+//                cdl.countDown()
+//            },
+//            sendBytes: { bytes ->
+//                ctx[0].schedule {
+//                    ctx[0].receiveBytes(bytes)
+//                }
+//            }
+//        ]
+//
+//        for(i in 0..100000)
+//            ctx[1].post(new RemoteMessageChannel.ForwardMessageToBeSent(forwardTo:RemoteMessageChannel.IOContext.MAIN_ACTOR_ID, message:ctx[1].mainActor))
+//
+//        cdl.await()
+//        pool0.shutdown()
+//        pool1.shutdown()
     }
 }
