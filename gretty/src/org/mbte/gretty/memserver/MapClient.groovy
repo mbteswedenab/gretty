5b81ada388e44dacec2e735aca71519ff4be69b5
Alex Tkachman <alex.tkachman@gmail.com>
2011-06-19 14:06:39 +0300

    copyright update

--- gretty/src/org/mbte/gretty/memserver/MapClient.groovy
+++ gretty/src/org/mbte/gretty/memserver/MapClient.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 MBTE Sweden AB.
+ * Copyright 2009-2011 MBTE Sweden AB.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package org.mbte.gretty.memserver
 
 import org.jboss.netty.channel.ExceptionEvent
